20eb2f9a6c3c4a46cbfb1a895665cfa085932192
matthias <matthias.fuchs>
2021-03-22 16:38:31 +0100

    json

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"sync"
@@ -16,8 +17,14 @@ type Client struct {
 }
 
 type Message struct {
-	Type int    `json:"type"`
-	Body string `json:"body"`
+	Type   int        `json:"type"`
+	Body   string     `json:"body,omitempty"`
+	Slider SliderMove `json:"slider,omitempty"`
+}
+
+type SliderMove struct {
+	SliderID     int     `json:"id, omitempty"`
+	PercentValue float32 `json:"percent, omitempty"`
 }
 
 func (c *Client) Read() {
@@ -33,7 +40,20 @@ func (c *Client) Read() {
 			return
 		}
 
-		message := Message{Type: messageType, Body: string(p)}
+		//bytes := []byte(string(p))
+		log.Printf(string(p))
+
+		//Check for slider
+		var sliderMove SliderMove
+		sliderError := json.Unmarshal(p, &sliderMove)
+		if sliderError != nil {
+			log.Println("no json")
+		}
+
+		// { "SliderID": 1, "PercentValue": 0.2}
+		// {"id": 1, "percent": 0.2}
+		//sliderMoveStatic := SliderMove{SliderID: 1, PercentValue: 0}
+		message := Message{Type: messageType, Body: string(p), Slider: sliderMove}
 		c.Pool.Broadcast <- message
 		fmt.Printf("Message Received: %+v\n", message)
 
